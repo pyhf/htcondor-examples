0b37cc4ba6e16ca6f77c86402dd58eedeea52e45
Matthew Feickert <matthew.feickert@cern.ch>
2022-12-02 18:28:59 -0600

    feat: Update Dockerfile to support non-root users (#4)
    * Create top level directories /.local /.config /.cache /.jupyter /work
      that are writeable by all users and make /usr/local/venv writable to
      allow for interactive use by non-root users.
    * Set WORKDIR to be all user writable /work directory.
    * Make 'build' nox session build the image for three common nvidia/cuda base images:
      nvidia/cuda:11.2.2-devel-ubuntu20.04, nvidia/cuda:11.6.0-cudnn8-devel-ubuntu20.04,
      nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04. Set the 11.6.0-cudnn8-devel-ubuntu20.04
      base image to be the default that 'latest-jax' is built against.

--- noxfile.py
+++ noxfile.py
@@ -15,30 +15,37 @@ def build(session):
     """
     Build image
     """
-    base_image = "nvidia/cuda:11.6.0-cudnn8-devel-ubuntu20.04"
+
+    default_image = "nvidia/cuda:11.6.0-cudnn8-devel-ubuntu20.04"
+    target_images = [
+        default_image,
+        "nvidia/cuda:11.2.2-devel-ubuntu20.04",
+        "nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04",
+    ]
     pyhf_version = "0.7.0"
     pyhf_backend = "jax"
-    cuda_version = base_image.split(":")[-1].split("-devel")[0]
+    for base_image in target_images:
+        cuda_version = base_image.split(":")[-1].split("-devel")[0]
 
-    session.run("docker", "pull", base_image, external=True)
-    session.run(
-        "docker",
-        "build",
-        "--file",
-        "docker/Dockerfile",
-        "--build-arg",
-        f"BASE_IMAGE={base_image}",
-        "--build-arg",
-        f"PYHF_VERSION={pyhf_version}",
-        "--build-arg",
-        f"PYHF_BACKEND={pyhf_backend}",
-        "--tag",
-        f"pyhf/cuda:{pyhf_version}-{pyhf_backend}-cuda-{cuda_version}",
-        "--tag",
-        f"pyhf/cuda:latest-{pyhf_backend}",
-        ".",
-        external=True,
-    )
+        session.run("docker", "pull", base_image, external=True)
+        run_command = [
+            "docker",
+            "build",
+            "--file",
+            "docker/Dockerfile",
+            "--build-arg",
+            f"BASE_IMAGE={base_image}",
+            "--build-arg",
+            f"PYHF_VERSION={pyhf_version}",
+            "--build-arg",
+            f"PYHF_BACKEND={pyhf_backend}",
+            "--tag",
+            f"pyhf/cuda:{pyhf_version}-{pyhf_backend}-cuda-{cuda_version}",
+        ]
+        if base_image == default_image:
+            run_command += ["--tag", f"pyhf/cuda:latest-{pyhf_backend}"]
+        run_command += "."
+        session.run(*run_command, external=True)
 
 
 @nox.session()
